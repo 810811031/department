c26cfc20fb44bfb8374503fe34ec00b089472e9d
luozhiyuan <810811031@qq.com>
2024-12-12 00:09:45 +0800

    feat: 首页和layout部分

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-    <app-layout v-if="useLayout">
-        <router-view />
-    </app-layout>
+    <n-dialog-provider>
+        <n-message-provider>
+            <app-layout v-if="useLayout">
+                <router-view />
+            </app-layout>
 
-    <router-view v-else />
+            <router-view v-else />
+        </n-message-provider>
+    </n-dialog-provider>
 </template>
 
 <script setup>
+import { NMessageProvider, NDialogProvider } from "naive-ui";
 import AppLayout from "@/components/Layout";
 import { computed } from "vue";
 import { RouterView, useRoute } from 'vue-router';
--- src/components/Layout/layout.vue
+++ src/components/Layout/layout.vue
@@ -6,12 +6,23 @@
             </div>
 
             <div class="layout-header-actions">
-
+                <div class="user">
+                    15997138967 您好!
+                    <n-icon size="large" style="margin-left: 10px;" :component="HappyOutline" />
+                </div>
+                
+                <n-icon size="large" class="logout" :component="PowerSharp" @click="handleLogout" />
             </div>
         </div>
 
         <div class="layout-menus">
-
+            <n-menu 
+                class="menu"
+                v-model:value="currentMenuName"
+                :options="menuOption"
+                :default-expanded-keys="['Finance']"
+                @update:value="handleUpdateMenu"
+            />
         </div>
 
         <div class="layout-content">
@@ -19,6 +30,74 @@
         </div>
     </div>
 </template>
+
+<script setup>
+import { NIcon, useDialog, NMenu } from 'naive-ui';
+import { 
+    HappyOutline, PowerSharp, ServerOutline, StatsChartOutline,
+    DocumentTextOutline, RecordingOutline, AlbumsOutline,
+} from "@vicons/ionicons5";
+import { useRouter, useRoute } from 'vue-router';
+import { ref, computed, h } from "vue";
+
+const dialog = useDialog();
+const router = useRouter();
+const route = useRoute();
+
+const currentMenuName = computed(() => {
+    return route.name
+});
+
+const menuOption = ref([
+    {
+        label: "数据中心",
+        key: "DataCenter",
+        icon: renderIcon(ServerOutline),
+    },
+    {
+        label: "财务",
+        key: "Finance",
+        icon: renderIcon(StatsChartOutline),
+        children: [
+            {
+                label: "财务对账",
+                key: "CheckAccounts",
+                icon: renderIcon(RecordingOutline),
+            },
+            {
+                label: "匹配记录",
+                key: "MatchRecords",
+                icon: renderIcon(DocumentTextOutline),
+            },
+            {
+                label: "凭证记录",
+                key: "ProofRecords",
+                icon: renderIcon(AlbumsOutline),
+            }
+        ]
+    }
+]);
+
+function renderIcon(icon) {
+    return () => h(NIcon, null, { default: () => h(icon) });
+}
+
+function handleUpdateMenu(name) {
+    router.push({ name });
+}
+
+function handleLogout() {
+    dialog.warning({
+        title: "退出确认",
+        content: "你确定要退出登录吗？",
+        positiveText: "确定",
+        negativeText: "取消",
+        onPositiveClick: () => {
+            router.replace({ name: "Login" });
+        },
+    });
+}
+</script>
 
 <style scoped lang="scss">
 .layout {
@@ -29,7 +108,8 @@
     flex-wrap: wrap;
     background-color: #f2f3f4;
     padding-top: 60px;
-    padding-left: 220px;
+    padding-left: 250px;
+    box-sizing: border-box;
     .layout-header {
         position: fixed;
         top: 0;
@@ -48,20 +128,47 @@
             font-size: var(--root-font-size-super);
             letter-spacing: 2px;
         }
+        .layout-header-actions {
+            display: flex;
+            align-items: center;
+            .user {
+                display: flex;
+                align-items: center;
+                color: #666;
+            }
+            .logout {
+                color: red;
+                margin-left: var(--root-padding-default);
+                padding: 3px;
+                cursor: pointer;
+                transition: all .2s linear;
+                    border-radius: var(--root-radius);
+                &:hover {
+                    background-color: rgba(0,0,0,.1);
+                    
+                }
+                &:active {
+                    background-color: rgba(0,0,0,.2);
+                }
+            }
+        }
     }
     .layout-menus {
         position: fixed;
         top: 60px;
         left: 0;
         display: inline-flex;
-        width: 220px;
+        width: 250px;
         height: calc(100% - 60px);
         box-shadow: var(--root-shadow);
         background-color: #fff;
+        .menu {
+            width: 100%;
+        }
     }
     .layout-content {
         display: flex;
-        width: calc(100% - 220px);
+        width: calc(100% - 250px);
         height: calc(100% - 60px);
         padding: var(--root-padding-default);
         box-sizing: border-box;
